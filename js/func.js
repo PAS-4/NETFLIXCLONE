7e7ffc1526a60dbf2202fcced0beb1c11adfb7c1
Patrese Ávila <64747567+PAS-4@users.noreply.github.com>
2021-08-14 00:30:24 -0300

    Alteração do nome das imagens (Não aparecem no link de visualização - "github pages")

--- js/func.js
+++ js/func.js
@@ -11,59 +11,59 @@ const titulo = document.querySelector('#titulo')
 const subtitulo = document.querySelector('#subtitulo')
 const sinopse = document.querySelector('#sinopse')
 
-function infoDefout(){
-    fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/defoutBack.jpg")'
-    fundoFilmeAtual.style.backgroundSize = 'cover'
-    subtitulo.innerHTML = 'Já recebeu a coruja com sua carta de Hogwarts?'
-    sinopse.innerHTML = 'Então embarque no Hogwarts Express - Plataforma 9 3/4'
-}
+// function infoDefaut(){
+//     fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/defautBack.jpg")'
+//     fundoFilmeAtual.style.backgroundSize = 'cover'
+//     subtitulo.innerHTML = 'Já recebeu a coruja com sua carta de Hogwarts?'
+//     sinopse.innerHTML = 'Então embarque no Hogwarts Express - Plataforma 9 3/4'
+// }
 
 function mudaInfoFilme(n){
     switch (n){
         case 1:
-            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp1-back.jpg")'
+            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp1back.jpg")'
             fundoFilmeAtual.style.backgroundSize = 'cover'
             subtitulo.innerHTML = 'e a Pedra Filosofal'
             sinopse.innerHTML = 'A magia começa aqui!!'
             break
         case 2:
-            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp2-back.jpg")'
+            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp2back.jpg")'
             fundoFilmeAtual.style.backgroundSize = 'cover'
             subtitulo.innerHTML = 'e a Câmara Secreta'
             sinopse.innerHTML = 'Um amigo elfo e um fantasma do passado!!'
             break
         case 3:
-            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp3-back.jpg")'
+            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp3back.jpg")'
             fundoFilmeAtual.style.backgroundSize = 'cover'
             subtitulo.innerHTML = 'e o Prisioneiro de Azkaban'
             sinopse.innerHTML = 'A Busca por justiça!!'
             break
         case 4:
-            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp4-back.jpg")'
+            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp4back.jpg")'
             fundoFilmeAtual.style.backgroundSize = 'cover'
             subtitulo.innerHTML = 'e o Cálice de Fogo'
             sinopse.innerHTML = 'Que os Jogos comecem (Torneio Tribruxo)!!'
             break
         case 5:
-            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp5-back.jpg")'
+            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp5back.jpg")'
             fundoFilmeAtual.style.backgroundSize = 'cover'
             subtitulo.innerHTML = 'e a Ordem da Fênix'
-            sinopse.innerHTML = 'A despedida de Sirius!!'
+            sinopse.innerHTML = 'A despedida de Sirius e a volta do Lord das Trevas!!'
             break
         case 6:
-            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp6-back.jpg")'
+            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp6back.jpg")'
             fundoFilmeAtual.style.backgroundSize = 'cover'
             subtitulo.innerHTML = 'e o Enigma do Príncipe'
             sinopse.innerHTML = 'A queda de Dumbledore!!'
             break
         case 7:
-            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp7-1-back.jpg")'
+            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp71back.jpg")'
             fundoFilmeAtual.style.backgroundSize = 'cover'
             subtitulo.innerHTML = 'e as Relíquias da Morte - Parte 1'
             sinopse.innerHTML = 'Hogwarts precisa de ajuda!!'
             break
         case 8:
-            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp7-2-back.jpg")'
+            fundoFilmeAtual.style.background = 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url("../img/hp72back.jpg")'
             fundoFilmeAtual.style.backgroundSize = 'cover'
             subtitulo.innerHTML = 'e as Relíquias da Morte - Parte 2'
             sinopse.innerHTML = 'Tudo termina aqui!!'
